740a444889d4669e3a6f91e9401ea439880d9d6d
provolot <provolot@gmail.com>
2013-11-07 18:24:33 -0500

    starting image blur process --- reading data from json file

---
+++ scripts/images_blur.py
@@ -0,0 +1,17 @@
+import json
+import weibo_settings
+from pprint import pprint
+
+json_filename = weibo_settings.deleted_log_json_filename 
+
+json_data=open(json_filename)
+
+data = json.load(json_data)
+
+json_data.close()
+
+postids = []
+for thispost in data:
+	postids.append(thispost["post_id"])
+
+print postids
